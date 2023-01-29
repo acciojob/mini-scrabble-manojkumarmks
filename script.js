3cd5bdda406ba8019af04238b8e547d909f2f66e
Manoj Kumar Sahoo <112025244+manojkumarmks@users.noreply.github.com>
2023-01-30 01:31:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your code here
+const textbox = document.getElementById("evaluatedText");
+const letterCount = document.getElementById("letterCount");
+textbox.addEventListener("input", () => {
+	letterCount.innerText = textbox.value.length;
+});
